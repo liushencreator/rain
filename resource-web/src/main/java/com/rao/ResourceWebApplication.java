b07b517cc17bce051662e7609886648597fd0551
raoxiaosi <1770561005@qq.com>
2019-08-11 22:09:32 +0800

    重新调整项目结构

--- resource-web/src/main/java/com/rao/ResourceWebApplication.java
+++ resource-web/src/main/java/com/rao/ResourceWebApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan(value = "com.rao.dao")
 @SpringBootApplication
-public class LocalWebApplication {
+public class ResourceWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LocalWebApplication.class, args);
+		SpringApplication.run(ResourceWebApplication.class, args);
 	}
 }
