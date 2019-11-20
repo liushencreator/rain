f7d709f7bdc01c4d1748d96547d29aacadaf2edf
raojing <1770561005@qq.com>
2019-11-20 11:46:08 +0800

    整合eurake注册中心

---
+++ resource-eurake/src/main/java/com/rao/ResourceEurakeApplication.java
@@ -0,0 +1,20 @@
+package com.rao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eurake 启动类
+ * @author raojing
+ * @date 2019/11/20 11:35
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class ResourceEurakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceEurakeApplication.class, args);
+    }
+    
+}
