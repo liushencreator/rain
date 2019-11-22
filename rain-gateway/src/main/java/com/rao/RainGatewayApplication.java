753421cdd45cb2d74ec0cb3b20d6ef80328fde90
raojing <1770561005@qq.com>
2019-11-22 15:55:40 +0800

    整合gateway 网关

---
+++ rain-gateway/src/main/java/com/rao/RainGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainGatewayApplication.class, args);
+    }
+
+}
