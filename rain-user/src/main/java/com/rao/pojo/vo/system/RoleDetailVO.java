841cdd7ce452cae07f21b066f5efd2ee09f67bcf
hudelin <736583887@qq.com>
2019-12-16 16:34:14 +0800

    代码优化，添加转换实体类工具

---
+++ rain-user/src/main/java/com/rao/pojo/vo/system/RoleDetailVO.java
@@ -0,0 +1,51 @@
+package com.rao.pojo.vo.system;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 角色详情 视图模型
+ * @author raojing
+ * @date 2019/12/12 14:46
+ */
+@Data
+public class RoleDetailVO {
+
+    /**
+     * id
+     */
+    @JsonSerialize(using= ToStringSerializer.class)
+    private Long id;
+
+    /**
+     * 角色名称
+     */
+    private String roleName;
+
+    /**
+     * 角色标识
+     */
+    private String roleCode;
+
+    /**
+     * 角色描述
+     */
+    private String roleDesc;
+
+    /**
+     * 创建时间
+     */
+//    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+//    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    private Date updateTime;
+    
+}
